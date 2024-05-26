7a93c99826bad0360b10e96242603138e8e43e82
Rxwestx <wexriexst13@gmail.com>
2024-05-26 19:31:12 +0900

    データを取得・表示するモジュールを作成

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { fetchHistoryData } from "./my-modules/fetch-history-data";
+import { submitData } from "./my-modules/submit-data";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-  authDomain: "daily-report-xxxx.firebaseapp.com",
-  projectId: "daily-report-xxxx",
-  storageBucket: "daily-report-xxxx.appspot.com",
-  messagingSenderId: "xxxxxxx",
-  appId: "xxxxxxxxxxxxxxxxxxxx",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID
 };
 
 // Initialize Firebase
@@ -21,42 +20,13 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 // Cloud Firestoreから取得したデータを表示する
-const fetchHistoryData = async () => {
-  let tags = "";
-
-  // reportsコレクションのデータを取得
-  const querySnapshot = await getDocs(collection(db, "reports"));
-
-  //データをテーブル表の形式に合わせてHTMLに挿入
-  querySnapshot.forEach((doc) => {
-    console.log(`${doc.id} => ${doc.data()}`);
-    tags += `<tr><td>${doc.data().date}</td><td>${doc.data().name}</td><td>${doc.data().work}</td><td>${doc.data().comment}</td></tr>`;
-  });
-  document.getElementById("js-history").innerHTML = tags;
-};
-
-// Cloud Firestoreから取得したデータを表示する
 if (document.getElementById("js-history")) {
   fetchHistoryData(getDocs, collection, db);
 }
-//Cloud Firestoreにデータを送信する
-const submitData = async (e) => {
-  e.preventDefault();
-const formData = new FormData(e.target);
-try {
-    const docRef = await addDoc(collection(db, "reports"), {
-      date: new Date(),
-      name: formData.get("name"),
-      work: formData.get("work"),
-      comment: formData.get("comment"),
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-};
 
 // Cloud Firestoreにデータを送信する
 if (document.getElementById("js-form")) {
-  document.getElementById("js-form").addEventListener("submit", (e) => submitData(e, addDoc, collection, db));
+  document
+    .getElementById("js-form")
+    .addEventListener("submit", (e) => submitData(e,addDoc, collection, db));
 }
